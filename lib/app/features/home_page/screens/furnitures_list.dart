dc0c94b28bdf1bcd8d3160579e9257cfb745847f
marc0410 <koffikanmarcephrem@gmail.com>
2026-02-16 03:51:13 +0000

    feat(furniture): ajouter la gestion des états et le cubit pour la récupération des meubles

--- lib/app/features/home_page/screens/furnitures_list.dart
+++ lib/app/features/home_page/screens/furnitures_list.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:gap/gap.dart';
+import 'package:immoplus/app/core/config/injection.dart';
+import 'package:immoplus/app/data/models/remote/furniture/furniture_model.dart';
+import 'package:immoplus/app/data/repositories/furniture_repository.dart';
 import 'package:immoplus/app/features/home_page/logic/home_page_state.dart';
-import 'package:immoplus/app/utils/app_colors.dart';
+import 'package:immoplus/app/utils/filter_handler.dart';
+import 'package:immoplus/app/widgets/tickets_cards/furniture_card.dart';
+import 'package:immoplus/app/widgets/tickets_cards/load_product_card.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class FurnituresList extends StatefulWidget {
@@ -14,22 +17,35 @@ class FurnituresList extends StatefulWidget {
 
 class _FurnituresListState extends State<FurnituresList> {
   Future<void> loadPage(int page) async {
-    // LogmentRepository.getResidences(page: page).then((value) {
-    //   if (value.hasNext == true) {
-    //     _pagingController.appendPage(
-    //         value.data ?? [], (value.currentPage)! + 1);
-    //   } else {
-    //     _pagingController.appendLastPage(value.data ?? []);
-    //   }
-    //   //change(value, status: RxStatus.success());
-    // }).onError((error, stackTrace) {
-    //   _pagingController.error = error.toString();
-    // });
+    final whereFilters = FilterHandler.getAllFilters(PropertyType.furniture);
+    FurnitureRepository furnitureRepository = getIt<FurnitureRepository>();
+    furnitureRepository
+        .getFurnitures(
+      search: FilterHandler.search,
+      lat: FilterHandler.lat,
+      long: FilterHandler.long,
+      startDate: FilterHandler.startDate,
+      endDate: FilterHandler.endDate,
+      where: whereFilters,
+      page: page,
+    )
+        .then((value) {
+      if (value.hasNext == true) {
+        HomePageState.pagingControllerFurniture
+            .appendPage(value.data ?? [], (value.currentPage)! + 1);
+      } else {
+        HomePageState.pagingControllerFurniture
+            .appendLastPage(value.data ?? []);
+      }
+    }).onError((error, stackTrace) {
+      HomePageState.pagingControllerFurniture.error = error.toString();
+    });
   }
 
   @override
   void initState() {
-    HomePageState.pagingControllerFurniture = PagingController(firstPageKey: 1);
+    HomePageState.pagingControllerFurniture =
+        PagingController(firstPageKey: 1);
     HomePageState.pagingControllerFurniture.addPageRequestListener((pageKey) {
       loadPage(pageKey);
     });
@@ -38,74 +54,42 @@ class _FurnituresListState extends State<FurnituresList> {
 
   @override
   void dispose() {
-    super.dispose();
-
     HomePageState.pagingControllerFurniture.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return SliverPadding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      sliver: SliverFillRemaining(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(right: 15),
-              child: Icon(
-                FontAwesomeIcons.couch,
-                size: 80,
-                color: Colors.grey,
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      sliver: PagedSliverList<int, FurnitureModel>(
+        pagingController: HomePageState.pagingControllerFurniture,
+        builderDelegate: PagedChildBuilderDelegate(
+          firstPageProgressIndicatorBuilder: (context) => Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              child: Column(
+                children: List.generate(
+                  10,
+                  (index) => LoadProductCard(),
+                ),
               ),
             ),
-            Text(
-              'Aucun éléments',
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: AppColors.primary),
+          ),
+          noItemsFoundIndicatorBuilder: (context) => Center(
+              child: Text(
+            "Aucun meuble trouvé",
+            style: Theme.of(context).textTheme.titleLarge,
+          )),
+          itemBuilder: (context, item, index) => Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 1).copyWith(bottom: 13),
+            child: FurnitureCard(
+              furniture: item,
             ),
-            const Gap(10),
-            Text(
-              "Actuellement, aucun meuble n'est disponible à la vente. Nous vous proposerons très bientôt une large sélection de magnifiques meubles que vous pourrez commander en toute simplicité.",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ],
+          ),
         ),
       ),
     );
-
-    // SliverPadding(
-    //   padding: const EdgeInsets.symmetric(horizontal: 9),
-    //   sliver: PagedSliverList<int, BienImmobilierModel>(
-    //     pagingController: HomePageState.pagingControllerFurniture,
-    //     builderDelegate: PagedChildBuilderDelegate(
-    //       firstPageProgressIndicatorBuilder: (context) => Padding(
-    //         padding: const EdgeInsets.all(10),
-    //         child: SizedBox(
-    //           //height: 600,
-    //           child: Column(
-    //             children: List.generate(
-    //               10,
-    //               (index) => LoadProductCard(),
-    //             ),
-    //           ),
-    //         ),
-    //       ),
-    //       noItemsFoundIndicatorBuilder: (context) => Center(
-    //           child: Text(
-    //         "Aucun élément trouvé",
-    //         style: Theme.of(context).textTheme.titleLarge,
-    //       )),
-    //       itemBuilder: (context, item, index) => EstateCard(
-    //         bienImmobilierModel: item,
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
